9b81a56c9eb3a6653f3cf6729e03f759ad715f06
boleyla1 <boleyla.mehrshad1@gmail.com>
2024-02-20 19:41:46 +0330

    update digi test

--- script.js
+++ script.js
@@ -5,9 +5,53 @@ $('#dasteh-bandi').mouseleave(function (){
     $('.master-div').fadeOut(100)
 })
 $("#right-img-story").click(function (){
-    $('.story-ul').animate({'margin-left':'+=200px'})
+    $('.story-ul').animate({'margin-left':'+=100px'})
 })
 $("#left-img-story").click(function (){
-    $('.story-ul').animate({'margin-left':'-=200px'})
+    $('.story-ul').animate({'margin-left':'-=100px'})
 
 })
+$('.Slider').mouseenter(function (){
+    $('.div-btn').fadeIn(100)
+})
+$('.Slider').mouseleave(function (){
+    $('.div-btn').fadeOut(100)
+})
+function Slider() {
+    if(nextslid>max){
+        nextslid=0;
+    }
+
+    slideritem.hide();
+    slideritem.eq(nextslid).fadeIn(100);
+    nextslid++;
+}
+function Slp() {
+    if(nextslid<min){
+        nextslid=2;
+    }
+
+    slideritem.hide();
+    slideritem.eq(nextslid).fadeIn(100);
+    nextslid--;
+}
+var max=2;
+var min=0;
+var slidertag =$('#Slider')
+var  slideritem=slidertag.find('.Item')
+var nextslid=0;
+$('#click-Right').click(function (){
+    Slider();
+
+
+});
+$('#click-Left').click(function (){
+    Slp();
+});
+
+
+
+Slider();
+var Timer = setInterval(function (){
+    Slider();
+},3500)
